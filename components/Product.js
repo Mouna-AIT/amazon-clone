f479f96829eeecbbf3f421aa05e65ff895285581
Mouna Ait <50374199+Mouna-AIT@users.noreply.github.com>
2021-12-19 16:33:22 -0500

    rounded the cards corners

--- components/Product.js
+++ components/Product.js
@@ -12,7 +12,7 @@ function Product({ id, title, price, description, category, image }) {
     );
     const [hasPrime] = useState(Math.random() > 0.5);
     return (
-        <div className= 'relative flex flex-col m-5 bg-white z-30 p-10'>
+        <div className= 'relative flex flex-col m-5 bg-white z-30 p-10 rounded-md'>
             <p className= 'absolute top-2 right-2 text-xs text-gray-400'>{category}</p>
             <Image src={image} height={200} width={200} objectFit="contain" />
 

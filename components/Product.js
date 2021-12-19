f895b38061728019fd0ab94961419d0e987240f5
Mouna Ait <50374199+Mouna-AIT@users.noreply.github.com>
2021-12-19 16:14:06 -0500

    added prime img and styled product components

--- components/Product.js
+++ components/Product.js
@@ -1,39 +1,43 @@
-import {useState} from 'react'
-import Image from 'next/image'
-import {StarIcon} from '@heroicons/react/solid'
+import { useState } from "react";
+import Image from "next/image";
+import { StarIcon } from "@heroicons/react/solid";
+import Currency from "react-currency-formatter";
 
 const MAX_RATING = 5;
 const MIN_RATING = 1;
 
-function Product({id, title, price, description, category, image}) {
-
-    const [rating]= useState(
+function Product({ id, title, price, description, category, image }) {
+    const [rating] = useState(
         Math.floor(Math.random() * (MAX_RATING - MIN_RATING + 1)) + MIN_RATING
     );
-const [hasPrime]= useState(Math.random() > 0.5)
+    const [hasPrime] = useState(Math.random() > 0.5);
     return (
-       
-        <div>
-            <p> 
-                {category }
-            </p>
-                <Image src={image} height={200} width={200} objectFit='contain'/>
+        <div className= 'relative flex flex-col m-5 bg-white z-30 p-10'>
+            <p className= 'absolute top-2 right-2 text-xs text-gray-400'>{category}</p>
+            <Image src={image} height={200} width={200} objectFit="contain" />
 
-                <h4>{title}</h4>
-                <div className= 'flex'>
-                    {Array(rating)
+            <h4 className= 'my-3'>{title}</h4>
+            <div className="flex">
+                {Array(rating)
                     .fill()
                     .map((_, i) => (
-<StarIcon className= 'h-5'/>
+                        <StarIcon className="h-5 text-amber-500" />
                     ))}
-            
             </div>
-<p>{description}</p>
-<div>
-    
-</div>
+            <p className= 'text-xs my-2 line-clamp-2'>{description}</p>
+            <div className='mb-5'>
+                <Currency quantity={price} currency="CAD" />
+            </div>
+            {hasPrime && (
+                <div className='flex items-center space-x-2 -mt-5 '>
+                    <img className= 'w-12' src="/Prime-tag.png" alt="" />
+                    <p className='text-xs text-gray-500'>FREE Next-day Delivey</p>
+                </div>
+            )}
+
+            <button className= 'mt-auto button'>Add To Basket</button>
         </div>
     );
 }
 
-export default Product
+export default Product;
